3198f6055af825ce81675028a41ebc5ee308a251
Steve Meredith <meredith.steve@gmail.com>
2023-03-12 12:16:03 -0700

    Add telescope file_browser extension and use for nvim config project

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -42,6 +42,7 @@ packer.init({
 return packer.startup(function(use)
     use { "wbthomason/packer.nvim" } -- Have packer manage itself
     use { "nvim-telescope/telescope.nvim", tag = "0.1.1", requires = { {"nvim-lua/plenary.nvim"} } }
+    use { "nvim-telescope/telescope-file-browser.nvim", requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" } }
     use { "folke/which-key.nvim" }
     use { "lewis6991/gitsigns.nvim" }
     use { "numToStr/Comment.nvim" }
--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -25,4 +25,4 @@ telescope.setup {
 
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
-
+pcall(require('telescope').load_extension, 'file_browser')
