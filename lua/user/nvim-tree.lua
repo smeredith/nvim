e71a4dae19b884d9ee5516ed38f7f89b714d76fa
Steve Meredith <meredith.steve@gmail.com>
2023-03-21 10:52:21 -0700

    Add c-f mapping to grep in a directory when tree is open

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -10,6 +10,22 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+-- live grep using Telescope inside the current directory under
+-- the cursor (or the parent directory of the current file)
+local function grep_in(node)
+  if not node then
+    return
+  end
+  local path = node.absolute_path or uv.cwd()
+  if node.type ~= 'directory' and node.parent then
+    path = node.parent.absolute_path
+  end
+  require('telescope.builtin').live_grep({
+    search_dirs = { path },
+    prompt_title = string.format('Grep in [%s]', vim.fs.basename(path)),
+  })
+end
+
 nvim_tree.setup {
   update_focused_file = {
     enable = true,
@@ -61,6 +77,7 @@ nvim_tree.setup {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
+        { key = '<C-f>', action = '', action_cb = grep_in, mode = 'n' },
       },
     },
   },
