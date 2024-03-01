c784ec3d8f562df40f327206f5d73c3948cbfa5d
Steve Meredith <meredith.steve@gmail.com>
2024-03-01 08:03:46 -0800

    Remove notebook hotkey

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -110,7 +110,6 @@ local leaderMappings = {
     k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
     m = { "<cmd>Telescope marks<cr>", "Marks" },
     M = { "<cmd>Telescope man_pages<cr>", "Man pages" },
-    n = { "<cmd>lua require('telescope.builtin').live_grep({cwd = '~/notebook'})<cr>", "Notebook" },
     l = { "<cmd>Telescope resume<cr>", "Resume last search" },
     r = { "<cmd>Telescope oldfiles<cr>", "Open recent file" },
     R = { "<cmd>Telescope registers<cr>", "Registers" },
